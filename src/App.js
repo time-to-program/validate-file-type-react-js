1f5297d4539504294d0fc71dbf01e8af7492b034
raviteja-v <timeforprogramming@gmail.com>
2023-05-15 19:42:08 +0530

    file validation code commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
+  const [file, setFile] = useState(null);
+
+  const [isError, setIsError] = useState(false)
+  const [errorMsg, setErrorMsg] = useState("");
+
+  const [isSuccess, setIsSuccess] = useState(false)
+
+  const handleFileChange = (event) => {
+    const selectedFile = event.target.files[0]; 
+    setIsSuccess(false)
+
+    // Checking if the file type is allowed or not
+    const allowedTypes = ["image/jpeg", "image/png", "image/gif"];
+    if (!allowedTypes.includes(selectedFile?.type)) {
+      setIsError(true)
+      setErrorMsg("Only JPEG, PNG, and GIF images are allowed.");
+      return;
+    }
+
+    setIsError(false)
+    setFile(selectedFile);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    if(isError) return
+    setErrorMsg("");
+
+    // Checking if the file has been selected
+    if (!file) {
+      setIsError(true)
+      setErrorMsg("Please select a file.");
+      return;
+    }
+
+    setIsError(false)
+    setIsSuccess(true)
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="card">
+        <div className="card-header">
+          <h2 className="title">File Type Validation</h2>
+        </div>
+
+        <div className="card-body">
+          <form onSubmit={handleSubmit}>
+            <input
+              type="file"
+              // accept=".jpg,.jpeg,.png,.gif"
+              onChange={handleFileChange}
+            />
+
+            {isError && <div className="error-text">{errorMsg}</div>}
+
+            <button type="submit">Upload</button>
+            
+            {isSuccess && <div className="success-text">Valid File Type</div>}
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
